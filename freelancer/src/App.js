43b5e2bdc66c52aa1b17c5eb0edc635ec05d1a3c
Cristhian muñoz <u1802342@unimilitar.edu.co>
2021-10-21 17:07:51 -0500

    Add Nav and Structure

---
+++ freelancer/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Nav from "./components/Nav";
+import Principal from "./components/Principal";
+function App() {
+  return (
+    <div className="App">
+      <Nav></Nav>
+      <Principal></Principal>
+    </div>
+  );
+}
+
+export default App;
---
+++ freelancer/src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../css/Nav.css";
+export default function Nav() {
+  return (
+    <div className="nav-bg">
+      <div className="navegacion-Principal contenedor">
+        <a href="!#">Inicio</a>
+        <a href="!#">Nosotros</a>
+        <a href="!#">Clientes</a>
+        <a href="!#">Contactanos</a>
+      </div>
+    </div>
+  );
+}
